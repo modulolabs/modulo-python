f7b9bf2cc1bf6dcf1c71ce45a8eb9930a0fb4a1c
Erin Tomson <erin@integerlabs.net>
2015-11-30 12:08:31 -0800

    Cleanup of port and connection classes

--- modulo/connection.py
+++ modulo/connection.py
@@ -1,3 +1,4 @@
+from __future__ import print_function, division, absolute_import, unicode_literals
 import serial
 
 class Port(object) :
@@ -22,6 +23,8 @@ class Port(object) :
 
     _BroadcastCommandExitBootloader = 100
 
+    _CodeEvent = ord('V')
+    
     def __init__(self, serialPortPath=None) :
         self._portInitialized = False
         self._lastAssignedAddress = 9
@@ -39,7 +42,6 @@ class Port(object) :
             s = s + chr(b)
         return s
 
-
     def findModuloByID(self, id) :
         for m in self._modulos :
             if (m._deviceID == id) :
@@ -51,37 +53,23 @@ class Port(object) :
             self.loop()
 
     def loop(self, noWait=False) :
-        if noWait :
-            if self._connection.inWaiting() == 0:
-                return
-
-        receiveData = self._connection.receivePacket()
-        if (receiveData == None) :
+        packet = self._connection.getNextPacket(noWait)
+        if (packet == None) :
             return False
-        if (receiveData[0] != 'V') :
-            self.processOutOfBandPacket(receiveData)
-            return False
+        if (packet[0] == self._CodeEvent) :
+            event = packet[1:]
 
-        print receiveData
+            eventCode = event[0]
+            deviceID = event[1] | (event[2] << 8)
+            eventData = event[3] | (event[4] << 8)
 
-        event = [ord(x) for x in receiveData[1:]]
-        #event = self._connection.transfer(self._BroadcastAddress, self._BroadcastCommandGetEvent, [], 5)
-        #if event is None or len(event) == 0:
-    #        break
-        #self._connection.transfer(self._BroadcastAddress, self._BroadcastCommandClearEvent, event, 0)
-
-    
-        eventCode = event[0]
-        deviceID = event[1] | (event[2] << 8)
-        eventData = event[3] | (event[4] << 8)
-
-        print event
-        
-        m = self.findModuloByID(deviceID)
-        if m :
-            print event
-            gotEvent = True
-            m._processEvent(eventCode, eventData)
+            print('Event', deviceID, eventCode, eventData)
+            
+            m = self.findModuloByID(deviceID)
+            if m :
+                m._processEvent(eventCode, eventData)
+        else :
+            print('Packet: ', packet)
 
         return True
 
@@ -106,10 +94,22 @@ class Port(object) :
         sendData = [nextDeviceID & 0xFF, nextDeviceID >> 8]
         resultData = self._connection.transfer(
             self._BroadcastAddress, self._BroadcastCommandGetNextDeviceID, sendData, 2)
-        if resultData is None or len(resultData) < 2:
-            return None
-        return resultData[1] | (resultData[0] << 8)
+        if resultData:
+            return resultData[1] | (resultData[0] << 8)
 
+    # Returns the device ID of the device on the port with the
+    # next greater ID than the one provided.
+    def getNextUnassignedDeviceID(self, lastDeviceID) :
+        if lastDeviceID == 0xFFFF :
+            return 0xFFFF
+        
+        nextDeviceID = lastDeviceID+1
+
+        sendData = [nextDeviceID & 0xFF, nextDeviceID >> 8]
+        resultData = self._connection.transfer(
+            self._BroadcastAddress, self._BroadcastCommandGetNextUnassignedDeviceID, sendData, 2)
+        if resultData :
+            resultData[1] | (resultData[0] << 8)
 
     def setAddress(self, deviceID, address) :
         sendData = [deviceID & 0xFF, deviceID >> 8, address]
@@ -120,13 +120,12 @@ class Port(object) :
         sendData = [deviceID & 0xFF, deviceID >> 8]
         retval = self._connection.transfer(self._BroadcastAddress, self._BroadcastCommandGetAddress,
             sendData, 1)
-        if retval is None :
-            return None
-        return retval[0]
+        if retval :
+            return retval[0]
 
     def setStatus(self, deviceID, status) :
         sendData = [deviceID & 0xFF, deviceID >> 8, status]
-        resultData = self._connection.transfer(
+        self._connection.transfer(
             self._BroadcastAddress, self._BroadcastCommandSetStatusLED, sendData, 0)
 
     def getVersion(self, deviceID) :
@@ -145,15 +144,19 @@ class Port(object) :
 
 
 class SerialConnection(object) :
-    Delimeter = 0x7E
-    Escape = 0x7D
+    _Delimeter = 0x7E
+    _Escape = 0x7D
+
+    _CodeEcho = ord('X')
+    _CodeTransfer = ord('T')
+    _CodeReceive = ord('R')
+    _CodeQuit = ord('Q')
 
     def __init__(self, path=None, controller=0) :
         super(SerialConnection, self).__init__()
 
-        from serial.tools import list_ports
-
         if path is None :
+            from serial.tools import list_ports
 
             # Modulo Controller will contain in the hardware description:
             #    "16d0:a67" on OSX
@@ -169,85 +172,85 @@ class SerialConnection(object) :
             raise IOError("Couldn't find a Modulo Controller connected via USB")
 
         self._serial = serial.Serial(path, timeout=.1)
-        self._eventData = []
+        self._outOfBandPackets = []
 
-        while not self.receivePacket() :
-            self.sendPacket([ord('X')])
-            pass
+        # The arduino samd usb serial implementation seems to swallow some
+        # initial data when the connection is being set up. To work around this,
+        # send pings until we get a response. After that the connection will be
+        # reliable.
+        while not self.getNextPacket() :
+            self.sendPacket([self._CodeEcho])
 
     def sendPacket(self, data) :
-        print 'Sending', data
-        self._serial.write(chr(self.Delimeter))
+    
+        packet = []
         for x in data :
-            if x == self.Delimeter or x == self.Escape :
-                self._serial.write(chr(self.Escape))
-                self._serial.write(chr(x ^ (1 << 5)))
+            if x == self._Delimeter or x == self._Escape :
+                packet.append(self._Escape)
+                packet.append(x ^ (1 << 5))
             else :
-                self._serial.write(chr(x))
-        self._serial.write(chr(self.Delimeter))
-        
+                packet.append(x)
 
-    def receivePacket(self) :
-        c = self._serial.read()
-        if (c == '') :
-            return None
-        while (c != chr(self.Delimeter) and c != '') :
-            print 'Skipping before delimeter: ', c
-            c = self._serial.read()
-
-        while (c == chr(self.Delimeter)) :
-            c = self._serial.read()
-
-        data = []
-        while (c != chr(self.Delimeter) and c != '') :
-            if (c == chr(self.Escape)) :
-                c = chr(ord(self._serial.read()) ^ (1 << 5))
-            data.append(c)
-            c = self._serial.read()
-
-        print 'Received', data
-        return data
-
-    def inWaiting(self) :
-        return self._serial.inWaiting()
+        self._serial.write([self._Delimeter] + packet + [self._Delimeter])
 
     def transfer(self, address, command, sendData, receiveLen) :
         if address is None :
             return None
 
-        sendBuffer = [ord('T'), address, command, len(sendData), receiveLen] + sendData
+        sendBuffer = [self._CodeTransfer, address, command, len(sendData),
+            receiveLen] + sendData
 
         self.sendPacket(sendBuffer)
 
-        receiveData = self.receivePacket()
-        while (receiveData != None and receiveData[0] != 'R') :
-            self.processOutOfBandPacket(receiveData)
-            receiveData = self.receivePacket()
+        # Receive packets and queue them in the out of band packet list until
+        # we receive a response packet.
+        receiveData = self._receivePacket()
+        while (receiveData != None and receiveData[0] != self._CodeReceive) :
+            self._outOfBandPackets.append(receiveData)
+            receiveData = self._receivePacket()
 
         if receiveData is None :
             return None
 
-        receiveData = [ord(x) for x in receiveData]
         return receiveData[2:]
 
+    def getNextPacket(self, noWait=False) :
+        if noWait and (not self._outOfBandPackets) and (self._connection.inWaiting() == 0):
+            return None
 
-    def processOutOfBandPacket(self, data) :
-        if (data[0] == 'D') :
-            print('Debug: ' + "".join(data[1:]))
+        if self._outOfBandPackets :
+            p = self._outOfBandPackets[0]
+            self._outOfBandPackets = self._outOfBandPackets[1:]
+            return p
         else :
-            print('Out of band: ', data)
-
-    def retrieveEventData(self) :
-        if (self._serial.inWaiting()) :
-            code = self._serial.read(1)
-            while (code == 'E') :
-                self._eventData.append(self._serial.read(5))
-                code = self._serial.read(1)
-        eventData = self._eventData
-        self._eventData = []
-        return eventData
-
+            return self._receivePacket()
 
     def close(self) :
-        self.sendPacket([ord('Q')])
-        self._serial.flush();    
+        self.sendPacket([self._CodeQuit])
+        self._serial.flush();
+
+    def _readByte(self) :
+        c = self._serial.read()
+        if c:
+            return ord(c)
+
+    def _receivePacket(self) :
+        c = self._readByte()
+        if (c is None) :
+            return None
+
+        while (c != self._Delimeter and c is not None) :
+            print('Skipping before delimeter: ', c)
+            c = self._readByte()
+
+        while (c == self._Delimeter) :
+            c = self._readByte()
+
+        data = []
+        while (c != self._Delimeter and c is not None) :
+            if (c == self._Escape) :
+                c = self._readByte() ^ (1 << 5)
+            data.append(c)
+            c = self._readByte()
+
+        return data
